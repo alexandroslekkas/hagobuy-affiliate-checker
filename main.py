799b1e443d9125f3efaada9848cf99a47578587b
Alexandros Lekkas <alexandros.lekkas@email.com>
2024-03-31 21:15:52 +0300

    Changed messages

--- main.py
+++ main.py
@@ -8,6 +8,29 @@ from datetime import datetime
 def main():
     asyncio.run(send_notification("Hey, I'm ready to begin tracking!"))
     previous_available_bonus_balance, previous_unsettled_amount, previous_bonus_earned, previous_number_of_invited, previous_recommended_order = 0, 0, 0, 0, 0
+    
+    available_bonus_balance, unsettled_amount, bonus_earned, number_of_invited, recommended_order = get_affiliate_statistics()
+    print("[>] First run")
+
+    now = datetime.now()
+
+    otification_message = """
+    ⬇️ Current Stats (At Start) ⬇️ {}
+    💵 Available Bonus Balance: ${}\n
+    💸 Unsettled Amount: ${}\n
+    🎉 Bonus Earned: ${}\n
+    🧑‍🤝‍🧑 Number of Invited: {}\n
+    📈 Recommended Order: {}
+    """.format(
+        (now.hour, now.day, now.month),
+        available_bonus_balance,
+        unsettled_amount,
+        bonus_earned,
+        number_of_invited,
+        recommended_order,
+        )
+
+    asyncio.run(send_notification(notification_message))
 
     while True:
         print("[>] Looooop!")
@@ -16,56 +39,32 @@ def main():
 
         available_bonus_balance_difference, unsettled_amount_difference, bonus_earned_difference, number_of_invited_difference, recommended_order_difference = available_bonus_balance-previous_available_bonus_balance, unsettled_amount-previous_unsettled_amount, bonus_earned-previous_bonus_earned, number_of_invited-previous_number_of_invited, recommended_order-previous_recommended_order
 
-        if (available_bonus_balance != available_bonus_balance_difference):
-            print("[>] Sending message")
+        print("[>] Sending message")
 
-            now = datetime.now()
+        now = datetime.now()
 
-            notification_message = """
-            ⬇️ Last Hour ⬇️ {}
-            💵 Available Bonus Balance: ${} (+${})\n
-            💸 Unsettled Amount: ${} (+${})\n
-            🎉 Bonus Earned: ${} (+${})\n
-            🧑‍🤝‍🧑 Number of Invited: {} (+{})\n
-            📈 Recommended Order: {} (+{})
-            """.format(
-                (now.hour, now.day, now.month),
-                available_bonus_balance,
-                available_bonus_balance_difference,
-                unsettled_amount,
-                unsettled_amount_difference,
-                bonus_earned,
-                bonus_earned_difference,
-                number_of_invited,
-                number_of_invited_difference,
-                recommended_order,
-                recommended_order_difference
+        notification_message = """
+        ⬇️ Last Hour ⬇️ {}
+        💵 Available Bonus Balance: ${} (+${})\n
+        💸 Unsettled Amount: ${} (+${})\n
+        🎉 Bonus Earned: ${} (+${})\n
+        🧑‍🤝‍🧑 Number of Invited: {} (+{})\n
+        📈 Recommended Order: {} (+{})
+        """.format(
+            (now.hour, now.day, now.month),
+            available_bonus_balance,
+            available_bonus_balance_difference,
+            unsettled_amount,
+            unsettled_amount_difference,
+            bonus_earned,
+            bonus_earned_difference,
+            number_of_invited,
+            number_of_invited_difference,
+            recommended_order,
+            recommended_order_difference
             )
 
-            asyncio.run(send_notification(notification_message))
-        else:
-            print("[>] First run")
-
-            now = datetime.now()
-
-            notification_message = """
-            ⬇️ Current Stats (At Start) ⬇️ {}
-            💵 Available Bonus Balance: ${}\n
-            💸 Unsettled Amount: ${}\n
-            🎉 Bonus Earned: ${}\n
-            🧑‍🤝‍🧑 Number of Invited: {}\n
-            📈 Recommended Order: {}
-            """.format(
-                (now.hour, now.day, now.month),
-                available_bonus_balance,
-                unsettled_amount,
-                bonus_earned,
-                number_of_invited,
-                recommended_order,
-            )
-
-            asyncio.run(send_notification(notification_message))
-
+        asyncio.run(send_notification(notification_message))
 
         available_bonus_balance, unsettled_amount, bonus_earned, number_of_invited, recommended_order = previous_available_bonus_balance, previous_unsettled_amount, previous_bonus_earned, previous_number_of_invited, previous_recommended_order
         
