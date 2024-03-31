2ab2da5d8e88ed5e4279b297b5ea8f3795d29670
Alexandros Lekkas <alexandros.lekkas@email.com>
2024-03-31 20:08:11 +0300

    Added output messages

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ def main():
     previous_available_bonus_balance, previous_unsettled_amount, previous_bonus_earned, previous_number_of_invited, previous_recommended_order = 0, 0, 0, 0, 0
 
     while True:
+        print("[>] Looooop!")
+
         available_bonus_balance, unsettled_amount, bonus_earned, number_of_invited, recommended_order = get_affiliate_statistics()
 
         available_bonus_balance_difference, unsettled_amount_difference, bonus_earned_difference, number_of_invited_difference, recommended_order_difference = available_bonus_balance-previous_available_bonus_balance, unsettled_amount-previous_unsettled_amount, bonus_earned-previous_bonus_earned, number_of_invited-previous_number_of_invited, recommended_order-previous_recommended_order
 
         if (available_bonus_balance != available_bonus_balance_difference):
+            print("[>] Sending message")
+
             now = datetime.now()
 
             notification_message = """
@@ -39,6 +43,27 @@ def main():
             )
 
             asyncio.run(send_notification(notification_message))
+        else:
+            print("[>] First run")
+
+            notification_message = """
+            ⬇️ Current Stats (At Start) ⬇️ {}
+            💵 Available Bonus Balance: ${}\n
+            💸 Unsettled Amount: ${}\n
+            🎉 Bonus Earned: ${}\n
+            🧑‍🤝‍🧑 Number of Invited: {}\n
+            📈 Recommended Order: {}
+            """.format(
+                (now.hour, now.day, now.month),
+                available_bonus_balance,
+                unsettled_amount,
+                bonus_earned,
+                number_of_invited,
+                recommended_order,
+            )
+
+            asyncio.run(send_notification(notification_message))
+
 
         available_bonus_balance, unsettled_amount, bonus_earned, number_of_invited, recommended_order = previous_available_bonus_balance, previous_unsettled_amount, previous_bonus_earned, previous_number_of_invited, previous_recommended_order
         
