4599311b2820141189f29b3f4139eda68390ea95
Alexandros Lekkas <alexandros.lekkas@email.com>
2024-04-03 12:13:28 +0300

    Updating requirements

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from services.statistics.statistics import statistics
 if __name__ == '__main__':
     print(threading.active_count())
     
-    telegram_bot_thread = threading.Thread(target=telegram_bot, args=())
-    telegram_bot_thread.start()
-    
     statistics_thread = threading.Thread(target=statistics, args=())
     statistics_thread.start()
+    
+    telegram_bot_thread = threading.Thread(target=telegram_bot, args=())
+    telegram_bot_thread.start()
     
     print(threading.active_count())
